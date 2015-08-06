f85c740c5e4905af76d1dfa1442aaf5240d885bc
Piper Merriam <pipermerriam@gmail.com>
2015-08-06 09:32:37 -0600

    intital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ethereum BloomFilter
+
+A [Bloom Filter](https://en.wikipedia.org/wiki/Bloom_filter) implementation in
+Ethereum.
---
+++ bloom.sol
@@ -0,0 +1,6 @@
+//sol Bloom Filter
+// @authors
+// Piper Merriam <pipermerriam@gmail.com>
+// A contract which implements a bloom filter.
+contract BloomFilter {
+}
